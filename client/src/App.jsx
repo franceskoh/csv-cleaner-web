8b20dd3b7dea0fad9aa527e19d89b4c0838119bb
franceskoh <franceskohasipi03@gmail.com>
2026-04-21 01:09:19 +0200

    Update API URL for production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     formData.append('file', file);
 
     try {
-      const response = await fetch('http://localhost:3000/upload', {
+      const response = await fetch('https://csv-cleaner-web-zti0.onrender.com/upload', {
         method: 'POST',
         body: formData,
       });
